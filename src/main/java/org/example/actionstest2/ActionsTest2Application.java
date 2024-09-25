11f0536216cf00d56a9c1b26a3844e3128aa3ed7
Jhockinn <Tobiln@hotmail.dk>
2024-09-25 09:33:16 +0200

    Initial commit

---
+++ src/main/java/org/example/actionstest2/ActionsTest2Application.java
@@ -0,0 +1,13 @@
+package org.example.actionstest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionsTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActionsTest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/actionstest2/ActionsTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.actionstest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
